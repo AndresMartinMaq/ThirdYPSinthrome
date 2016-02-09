732083b7a256af542117e64fe28a7ab049cfa19a
AndresMartinMaq <andrewtri800@hotmail.com>
2016-02-09 13:58:11 +0000

    Database Structure complete 1.0

--- app/src/main/java/com/example/andres/thirdypsinthrome/persistence/DBContract.java
+++ app/src/main/java/com/example/andres/thirdypsinthrome/persistence/DBContract.java
@@ -4,9 +4,8 @@ import android.provider.BaseColumns;
 
 public class DBContract {
 
-    
-
-    //Maybe shouldn't be used, as this could better be used as user preferences?
+    //TODO Maybe shouldn't be used, as this could better be used as user preferences?
+    //Implementing BaseColums will automatically generate a _id.
     public static final class UserTable implements BaseColumns{
         public static final String TABLE_NAME = "user";
 
@@ -14,11 +13,14 @@ public class DBContract {
         public static final String COL_TARGET_INR_MAX = "target_inr_max";
         //This med time will be stored as a String HH:MM.
         public static final String COL_MED_TIME = "med_taking_time";
+        //Foreign key that relates user to a medicine.
+        public static final String COL_MEDICINE_FK = "medicine_id";
     }
 
     public static final class DosageTable implements BaseColumns{
         public static final String TABLE_NAME = "dosage";
 
+        public static final String COL_USER_FK = "user_id";
         //These dates will be stored as Unix epoch integers.
         public static final String COL_START = "start_date";
         public static final String COL_END = "end_date";
@@ -29,9 +31,10 @@ public class DBContract {
         public static final String TABLE_NAME = "days";
 
         public static final String COL_DATE = "date";
-        public static final String COL_USER_ID = "user";
+        public static final String COL_DOSAGE_FK = "dosage_id";
+        public static final String COL_USER_ID = "user";    //This is unnecesary if we have a FK to dosage
         public static final String COL_MILLIGRAMS = "milligrams";
-        public static final String COL_TAKEN = "med_taking_time";    //boolean
+        public static final String COL_TAKEN = "med_taking_time";    //boolean, encoded as integer 0 or 1
         public static final String COL_DEVIATION = "dev_minutes";
         public static final String COL_NOTES = "patient_notes";      //string, can be null
     }
@@ -51,7 +54,7 @@ public class DBContract {
         public static final String TABLE_NAME = "dose_adjustment";
 
         public static final String COL_MEDICINE_ID = "medicine_id";
-        public static final String COL_INCR_OR_DECR = "increasing";//boolean, false indicates decreasing
+        public static final String COL_INCR_OR_DECR = "increasing";//boolean stored as int, 0 indicates decreasing, 1 increasing
         public static final String COL_LEVEL = "level";
         public static final String COL_DAY1 = "day1";
         public static final String COL_DAY2 = "day2";
@@ -62,6 +65,7 @@ public class DBContract {
     public static final class INRBacklogTable implements BaseColumns{
         public static final String TABLE_NAME = "inr_backlog";
 
+        public static final String COL_USER_FK = "user_id";
         public static final String COL_DATE_OF_TEST = "date";//date will act as a key to relate it to days table?
         public static final String COL_INR_VALUE = "inr";
     }
--- app/src/main/java/com/example/andres/thirdypsinthrome/persistence/DBHelper.java
+++ app/src/main/java/com/example/andres/thirdypsinthrome/persistence/DBHelper.java
@@ -18,12 +18,52 @@ public class DBHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //User table
         final String sqlStatementUserTable = "CREATE TABLE " + UserTable.TABLE_NAME + " (" +
                 UserTable._ID + " INTEGER PRIMARY KEY," +
                 UserTable.COL_TARGET_INR_MIN + " REAL, " +
                 UserTable.COL_TARGET_INR_MAX + " REAL, " +
+                " FOREIGN KEY (" + UserTable.COL_MEDICINE_FK + ") REFERENCES " + MedicineTable.TABLE_NAME + " (" + MedicineTable._ID + "), " +
                 UserTable.COL_MED_TIME + " TEXT DEFAULT '19:00' NOT NULL); ";
-        //TODO create other tables.
+        //Dosage table
+        final String sqlSmtDosage = "CREATE TABLE " + DosageTable.TABLE_NAME + " (" +
+                " FOREIGN KEY (" + DosageTable.COL_USER_FK + ") REFERENCES " + UserTable.TABLE_NAME + " (" + UserTable._ID + "), " +
+                DosageTable.COL_START + " INTEGER NOT NULL, " +
+                DosageTable.COL_END + " INTEGER NOT NULL, " +
+                DosageTable.COL_LEVEL + " INTEGER NOT NULL); ";
+        //Day table
+        final String sqlSmtDay = "CREATE TABLE " + DayTable.TABLE_NAME + " (" +
+                DayTable.COL_DATE + " INTEGER NOT NULL, " +
+                " FOREIGN KEY (" + DayTable.COL_DOSAGE_FK + ") REFERENCES " + DosageTable.TABLE_NAME + " (" + DosageTable._ID + "), " +
+                DayTable.COL_MILLIGRAMS + " REAL NOT NULL, " +
+                DayTable.COL_TAKEN + " INTEGER DEFAULT 0, " +
+                DayTable.COL_DEVIATION + " INTEGER, " +
+                DayTable.COL_NOTES + " TEXT); ";
+        //Medicine table
+        final String sqlSmtMedicine = "CREATE TABLE " + MedicineTable.TABLE_NAME + " (" +
+                MedicineTable.COL_COMMERCIAL_NAME + " TEXT NOT NULL, " +
+                MedicineTable.COL_MILLIGRAMS_PER_TABLET + " REAL NOT NULL);";
+        //DosageAdjustment table
+        final String sqlSmtDosageAdjust = "CREATE TABLE " + DosageAdjustmentTable.TABLE_NAME + " (" +
+                " FOREIGN KEY (" + DosageAdjustmentTable.COL_MEDICINE_ID + ") REFERENCES " + MedicineTable.TABLE_NAME + " (" + MedicineTable._ID + "), " +
+                DosageAdjustmentTable.COL_INCR_OR_DECR + " INTEGER NOT NULL, " +
+                DosageAdjustmentTable.COL_DAY1 + " REAL NOT NULL, " +
+                DosageAdjustmentTable.COL_DAY2 + " REAL, " +
+                DosageAdjustmentTable.COL_DAY3 + " REAL, " +
+                DosageAdjustmentTable.COL_DAY4 + " REAL, " +
+                DosageAdjustmentTable.COL_LEVEL + " INTEGER NOT NULL); ";
+        //Dosage table
+        final String sqlSmtINRBacklog = "CREATE TABLE " + INRBacklogTable.TABLE_NAME + " (" +
+                " FOREIGN KEY (" + INRBacklogTable.COL_USER_FK + ") REFERENCES " + UserTable.TABLE_NAME + " (" + UserTable._ID + "), " +
+                INRBacklogTable.COL_DATE_OF_TEST + " INTEGER NOT NULL, " +
+                INRBacklogTable.COL_INR_VALUE + " REAL NOT NULL); ";
+        //Execute the statements, creating the tables. Order matters, as tables with FKs need to created after.
+        db.execSQL(sqlSmtMedicine);
+            db.execSQL(sqlSmtDosageAdjust);
+        db.execSQL(sqlStatementUserTable);
+            db.execSQL(sqlSmtDosage);
+                db.execSQL(sqlSmtDay);
+        db.execSQL(sqlSmtINRBacklog);
     }
 
     @Override
